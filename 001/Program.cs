d625998c73e702b57834dd5cc09aa174baac2ad7
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-11 20:33:52 +0300

    task 001

---
+++ 001/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+int[] array = { 1, 22, 3, 94, 5, 36, 7, 48};
+int n = array.Length;
+int index = 0;
+
+while (index < n)
+{
+    Console.WriteLine(array[index]);
+    index++;
+}
